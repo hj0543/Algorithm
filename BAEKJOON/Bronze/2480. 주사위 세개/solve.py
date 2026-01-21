fffa6a9cd22aa4bc72408ee83e4527d12daeaeaf
hj0543 <hj0543@gmail.com>
2026-01-22 08:39:40 +0900

    2480 update

--- BAEKJOON/Bronze/2480. 주사위 세개/solve.py
+++ BAEKJOON/Bronze/2480. 주사위 세개/solve.py
@@ -1,6 +1,18 @@
 A, B, C = map(int, input().split())
 
-
+if A == B == C:
+    print(10000 + 1000 * A)
+elif (A == B) or (C == A):
+    print(1000 + 100 * A)
+elif B == C:
+    print(1000 + 100 * B)
+else:
+    if A > B and A > C:
+        print(A * 100)
+    elif B > A and B > C:
+        print(B * 100)
+    else:
+        print(C * 100)
 
 
 
