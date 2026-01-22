afe787e617700167fb73cfd2f711a04bd5fab4e4
hj0543 <hj0543@gmail.com>
2026-01-22 13:02:49 +0900

    10950, 25304, 8393 add

---
+++ BAEKJOON/Bronze/25304. 영수증/solve.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+X = int(input())
+N = int(input())
+
+add = 0
+
+for i in range(N):
+    a, b = map(int, input().split())
+    add += a * b
+
+if X == add:
+    print('Yes')
+else:
+    print('No')
