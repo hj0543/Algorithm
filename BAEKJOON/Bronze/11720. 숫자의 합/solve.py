332e8fa10047d195d175940e48dedacac2915b0d
hj0543 <hj0543@gmail.com>
2026-01-24 00:14:26 +0900

    11720 add

--- BAEKJOON/Bronze/11720. 숫자의 합/solve.py
+++ BAEKJOON/Bronze/11720. 숫자의 합/solve.py
@@ -1,4 +1,16 @@
-N = int(input())
-A = int(input())
+import sys
+sys.stdin = open('input.txt', 'r')
 
-B = list(A)
+N = int(input())
+A = input().strip() # list로 받으려면 int 쓰지 말 것.
+
+arr = list(map(int, A))
+
+total = 0
+
+for i in range(0, N):
+    total += arr[i]
+
+print(total)
+
+# for i in listed_A:
