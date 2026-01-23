dc2c4bad97a8a44b9154dd5200a7cc7682176be7
hj0543 <hj0543@gmail.com>
2026-01-23 11:19:16 +0900

    fixed

--- BAEKJOON/Bronze/5597. 과제 안 낸 사람/solve.py
+++ BAEKJOON/Bronze/5597. 과제 안 낸 사람/solve.py
@@ -5,7 +5,7 @@ import sys
 
 checked_nums = list(map(int, sys.stdin.buffer.read().split()))
 
-nums = list(range(1, 30))
+nums = list(range(1, 31)) #(1, 30)하면 틀림
 
 absence_nums = sorted(set(nums) - set(checked_nums))
 
