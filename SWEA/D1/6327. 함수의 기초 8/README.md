87fbcbacb82d8f49cfd19babf64ba668c031905c
hj0543 <hj0543@gmail.com>
2026-01-25 23:58:16 +0900

    26.01.25

---
+++ SWEA/D1/6327. 함수의 기초 8/README.md
@@ -0,0 +1,9 @@
+[문제링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWcWMJsq5d4DFAU4)
+
+### 풀이 결과
+- 메모리 : 50,304 kb
+- 실행시간 : 59 ms
+- 코드길이 : 149
+
+### 제출 일자
+2026년 1월 25일 23:57
---
+++ SWEA/D1/6327. 함수의 기초 8/solve.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+A, B = map(int, input().split(','))
+
+def square (x):
+    result = x ** 2
+    return result
+
+for i in (A, B):
+    print(f'square({i}) => {square(i)}')
