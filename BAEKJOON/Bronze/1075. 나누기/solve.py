4d577c56cfd0c28be38f419e8dda0b3f08100b11
hj0543 <hj0543@gmail.com>
2026-01-27 07:21:08 +0900

    BOJ 1075 add

--- BAEKJOON/Bronze/1075. 나누기/solve.py
+++ BAEKJOON/Bronze/1075. 나누기/solve.py
@@ -4,7 +4,15 @@ sys.stdin = open('input.txt', 'r')
 N = int(input())
 F = int(input())
 
+first = N // 100 * 100
 
-
+i = 0
+while True:
+    if (first + i) % F == 0:
+        number = str(first + i)
+        break
+    i += 1
+ 
+print(number[-2:])
 
 
