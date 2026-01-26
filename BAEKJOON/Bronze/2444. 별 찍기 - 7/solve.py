fe5cc76e5fd07ccef4d163f3e485af7845c33fc4
hj0543 <hj0543@gmail.com>
2026-01-27 00:53:07 +0900

    BOJ 2444 add

---
+++ BAEKJOON/Bronze/2444. 별 찍기 - 7/solve.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+N = int(input())
+
+i = 1
+while i <= N:
+    spaces = N - i
+    stars = 2 * i - 1
+    print(' ' * spaces + '*' * stars)
+    i += 1
+
+j = N - 1
+while j >= 1:
+    spaces = N - j
+    stars = 2 * j - 1
+    print(' ' * spaces + '*' * stars)
+    j -= 1
+    
+
+
