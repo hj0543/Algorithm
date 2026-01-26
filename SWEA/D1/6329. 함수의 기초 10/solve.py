f8a99ecf399a20b84c11866b579cac69be72402e
hj0543 <hj0543@gmail.com>
2026-01-26 22:39:15 +0900

    SWEA 6329 add

--- SWEA/D1/6329. 함수의 기초 10/solve.py
+++ SWEA/D1/6329. 함수의 기초 10/solve.py
@@ -4,7 +4,7 @@ def countdown (x):
         return '카운트다운을 하려면 0보다 큰 입력이 필요합니다.'
     else:       
         for i in range(1, x + 1):           
-            result.append(str(i))
+            result.append(str(i)) # str 변수
             reversed_result = result[::-1]
             results = '\n'.join(reversed_result)
     return results
