da983c72a7de11d16525b718659c2c757fd09c39
hj0543 <hj0543@gmail.com>
2026-01-27 07:34:48 +0900

    BOJ 1546 add

--- BAEKJOON/Bronze/1546. 평균/solve.py
+++ BAEKJOON/Bronze/1546. 평균/solve.py
@@ -10,10 +10,11 @@ max_score = sorted_scores[-1]
 
 jojak = []
 
-for i in range(N): # ()안에 뭘 넣어야 하지?
+for i in range(N):
     fixed_scores = scores[i] / max_score * 100
     jojak.append(fixed_scores)
 
 average = sum(jojak) / N
 print(average)
+
 
