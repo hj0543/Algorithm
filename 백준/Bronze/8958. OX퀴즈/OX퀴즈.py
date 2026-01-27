4293a7f902d8930a7735d0feb8510959f878db4e
Haneul <hj0543@gmail.com>
2026-01-28 07:18:23 +0900

    [Bronze II] Title: OX퀴즈, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+for _ in range(1, N + 1):
+    OX = list(input())
+
+    total_score = 0
+    current_score = 0
+    for i in range(len(OX)):
+        if OX[i] == 'O':
+            current_score += 1
+            total_score += current_score
+        else:
+            current_score = 0
+    
+    print(total_score)
