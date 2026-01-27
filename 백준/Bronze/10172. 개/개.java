84c0a4beb23752af9f944d502696112e82cdf05f
Haneul <hj0543@gmail.com>
2026-01-28 07:19:44 +0900

    [Bronze V] Title: 개, Time: 60 ms, Memory: 11220 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\"); // "를 표현하기 위해 \" 사용
+        System.out.println("|\"^\"`    |"); // "를 표현하기 위해 \" 사용
+        System.out.println("||_/=\\\\__|"); // \를 두 번 출력하기 위해 \\\\ 사용
+    }
+}
