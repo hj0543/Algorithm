dfd1b3b8059c683a6f214e61b2d372127d210f6e
hj0543 <hj0543@gmail.com>
2026-01-27 19:25:36 +0900

    BOJ 8958 add

--- BAEKJOON/Bronze/8958. OX퀴즈/solve.py
+++ BAEKJOON/Bronze/8958. OX퀴즈/solve.py
@@ -4,10 +4,17 @@ sys.stdin = open('input.txt', 'r')
 N = int(input())
 
 
-total = 0
-# for _ in range(1, N + 1):
-#     OX = list(input())
 
-#     total = 0
-#     for i in range(len(OX)):
-#         if OX[i] == 'O':
+for _ in range(1, N + 1):
+    OX = list(input())
+
+    total_score = 0
+    current_score = 0
+    for i in range(len(OX)):
+        if OX[i] == 'O':
+            current_score += 1
+            total_score += current_score
+        else:
+            current_score = 0
+    
+    print(total_score)
