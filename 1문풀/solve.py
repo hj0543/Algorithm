57c5b1c39ba06ba7d025dc779bea5e9e4d7c4fe6
hj0543 <hj0543@gmail.com>
2026-01-30 01:28:56 +0900

    go to sleep

--- 1문풀/solve.py
+++ 1문풀/solve.py
@@ -1,30 +1,22 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
+import sys
+input = sys.stdin.readline
 
-T = int(input())
+A, B, N = map(int, input().split())
 
-for _ in range(T):
-    init = list(map(int, input().split()))
-
-##############################################################
-
-x1 = []
-y1 = []
+# * 10^n n => 5   | 10^n 일의 자리에 해당하는 숫자가 소수 n의 자리가 된다. 
 
 
-for i in range(init[0]):
-    for j in range(init[1]):            
-        rang = (i - init[0] ** 2 + j - init[1] ** 2) ** 0.5
-        if rang <= init[2]:
+num1 = A * (10 ** N) # A에 10의 N제곱만큼 곱한다.
 
-        
+num2 = num1 // B # num1에서 B의 몫나눗셈을 한다. (소숫점 자를거라서)
 
+result = str(num2)[-1] # 마지막에 있는 수가 소수 N번째 자릿수
 
-
-
-
-
+print(result)
+ 
 
 
 
