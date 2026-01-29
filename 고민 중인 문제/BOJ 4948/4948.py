0aad7937963040296394c6dd22a792443791f696
hj0543 <hj0543@gmail.com>
2026-01-30 08:27:06 +0900

    commit check

--- 고민 중인 문제/BOJ 4948/4948.py
+++ 고민 중인 문제/BOJ 4948/4948.py
@@ -11,25 +11,25 @@ input = sys.stdin.readline
 
 ##################################################################
 
-# import math
+import math
 
-# def is_prime(n):
-#     if n < 2:
-#         return False
-#     # 2부터 n의 제곱근까지 나누어 봄
-#     for i in range(2, int(math.sqrt(n)) + 1):
-#         if n % i == 0:
-#             return False # 소수가 아님
-#     return True # 소수임
-
-
-def is_prime_simple(n):
-    if n < 2: 
+def is_prime(n):
+    if n < 2:
         return False
-    for i in range(2, n):
+    # 2부터 n의 제곱근까지 나누어 봄
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
-            return False
-    return True
+            return False # 소수가 아님
+    return True # 소수임
+
+
+# def is_prime_simple(n):
+#     if n < 2: 
+#         return False
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return False
+#     return True
 
 ########################################################################
 
@@ -42,7 +42,7 @@ while True:
 
 
     for i in range(n + 1, (2 * n) + 1): # 임의의 정수 i가 초과 2n미만의 범위에서
-        if is_prime_simple(i) == True: # i가 소수라면
+        if is_prime(i) == True: # i가 소수라면
             cnt += 1 # 카운트를 한다.
         else: # 아니면 패스
             pass
