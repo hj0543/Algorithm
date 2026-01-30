a66177651246eaca451b1681200a93a86884a15d
Haneul <hj0543@gmail.com>
2026-01-30 22:01:01 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 960 ms, Memory: 142080 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -8,8 +8,7 @@ numbers = [] # sort쓸거니까 빈 리스트에 저장하고
 for _ in range(N):
     numbers.append(int(sys.stdin.readline()))
 
-sort_nums = sorted(numbers) # sorted 사용해서 오름차순 정렬
+numbers.sort()  # sorted() 대신 in-place 정렬도 OK
 
-
-for j in range(len(sort_nums)):
-    print(sort_nums[j])
+output = '\n'.join(map(str, numbers))
+sys.stdout.write(output)
