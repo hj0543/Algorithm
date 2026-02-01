0aabe312208c6a301dca085c5fe96b443cdb67b6
hj0543 <hj0543@gmail.com>
2026-02-01 16:21:46 +0900

    commit check

--- 3문풀/solve.py
+++ 3문풀/solve.py
@@ -1,2 +1,17 @@
 import sys
 sys.stdin = open('input.txt', 'r')
+
+# 4948
+# import sys
+# input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+for _ in range(M):
+    change = list(map(int, input().split()))
+
+
+    print(change)
+
+    
+
