120a3a8522a4b62beb7f27ebfa0b4f400b71ba15
hj0543 <hj0543@gmail.com>
2026-02-02 17:56:15 +0900

    exit

--- 3문풀/solve.py
+++ 3문풀/solve.py
@@ -5,7 +5,16 @@ sys.stdin = open('input.txt', 'r')
 import sys
 input = sys.stdin.readline
 
+a, b, c, d, e, f = map(int, input().split())
 
+# x 값 구하기
+if a * e - d * b != 0:
+    x = (c * e - f * b) // (a * e - d * b)
 
-    
+# y 값 구하기
+if b != 0:
+    y = (c - a * x) // b
+else:
+    y = (f - d * x) // e
 
+print(x, y)
