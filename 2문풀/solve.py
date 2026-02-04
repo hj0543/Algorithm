08a4689a5cf18ed88e307d8a63f40f499b5318f0
hj0543 <hj0543@gmail.com>
2026-02-04 16:52:06 +0900

    exit

--- 2문풀/solve.py
+++ 2문풀/solve.py
@@ -5,13 +5,38 @@ sys.stdin = open('input.txt', 'r')
 import sys
 input = sys.stdin.readline
 
-A, B, C = map(int, input().split())
+N = int(input())
 
+def my_abs(a, b):
+    if a - b == - b:
+        result = 0
+    elif a - b >= 0:
+        result = a - b
+    else:
+        result = b - a
+    return result
 
+for _ in range(N):
+    T = int(input())
+    board = [list(map(int, input().split())) for _ in range(T)]
 
+    # 0으로 채워진 7 x 7 행렬 생성
+    grid = [[0] * 7 for _ in range(7)]
 
+    # 0으로 채워진 7 x 7 행렬에 불러온 행렬을 모서리를 비워둔채로 삽입
+    for i in range(5):
+        for j in range(5):
+            grid[i+1][j+1] = board[i][j]
 
-
-
+    total = 0
+    for i in range(1, T+1):
+        for j in range(1, T+1):
+            a = my_abs(grid[i][j+1], grid[i][j])
+            b = my_abs(grid[i-1][j], grid[i][j])
+            c = my_abs(grid[i+1][j], grid[i][j])
+            d = my_abs(grid[i][j-1], grid[i][j])
+            add = a + b + c + d
+            total += add
+    print(f'#{_+1} {total}')
 
 
--- 3문풀/solve.py
+++ 3문풀/solve.py
@@ -5,21 +5,50 @@ sys.stdin = open('input.txt', 'r')
 import sys
 input = sys.stdin.readline
 
-A = [] # 행렬
+def my_len(arr):
+    cnt = 0
+    for _ in arr:
+        cnt += 1
+    return cnt
 
-for _ in range(9):
-    A.append(list(map(int, input().split())))
+def my_sum(arr):
+    total = 0
+    for i in range(my_len(arr)):
+        total += arr[i]
+    return total
 
-max_number = 0 # 최대값 초기값 설정
-x = 0 # 최대값일때 행
-y = 0 # 최대값일때 열
+def my_max(arr):
+    max_num = arr[0]
+    for i in range(my_len(arr)):
+        if arr[i] > max_num:
+            max_num = arr[i]
+    return max_num
 
-for i in range(9):
-    for j in range(9):
-        if A[i][j] >= max_number: # 최대값 갱신
-            max_number = A[i][j]
-            x = i + 1
-            y = j + 1
+for _ in range(10):
+    N = int(input())
+    grid = [list(map(int, input().split())) for _ in range(100)]
 
-print(max_number)
-print(x, y)
+    total = []
+    for i in range(100):
+        total.append(my_sum(grid[i]))
+
+    for i in range(100):
+        add1 = 0
+        for j in range(100):
+            add1 += grid[j][i]
+        total.append(add1)
+
+
+    add2 = 0
+    for i in range(100):
+        add2 += grid[i][i]
+    total.append(add2)
+
+    add3 = 0
+    for i in range(100):
+        add3 += grid[i][99-i]
+    total.append(add3)
+
+    print(f'#{_+1} {my_max(total)}')
+
+
