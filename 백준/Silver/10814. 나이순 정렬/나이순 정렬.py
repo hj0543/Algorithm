067257ae3670be23558b6960036d8aee89c70ebc
hj0543 <hj0543@gmail.com>
2026-02-07 00:34:52 +0900

    [Silver V] Title: 나이순 정렬, Time: 188 ms, Memory: 48068 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+members = []
+
+for _ in range(N):
+    age, name = input().split()
+    members.append([int(age), name])
+
+members.sort(key=lambda x: (x[0]))
+
+for member in members:
+    print(*member)
