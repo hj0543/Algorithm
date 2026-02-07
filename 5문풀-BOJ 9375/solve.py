50cd80b1549bdc21255133a79de89c4a969fe178
hj0543 <hj0543@gmail.com>
2026-02-07 16:27:13 +0900

    go home

--- 5문풀-BOJ 9375/solve.py
+++ 5문풀-BOJ 9375/solve.py
@@ -7,12 +7,25 @@ import sys
 input = sys.stdin.readline
 
 TC = int(input())
+
+
 for tc in range(1, TC+1):
     n = int(input())
+    data = {}
+    # {'headgear': 2, 'eyewear': 1}
+    # {'face': 3}
+
     for i in range(n):
-        clothes = input()
+        name, type = map(str, input().split())
 
+        if type in data:
+            data[type] += 1
+        else:
+            data[type] = 1
 
+    # print(len(data))
+    for j in data.values():
+        print(j)
 
 
 
