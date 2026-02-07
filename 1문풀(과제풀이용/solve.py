994e1717242666e4ad288d5bd9d42118803796ad
hj0543 <hj0543@gmail.com>
2026-02-07 22:07:52 +0900

    sleep

--- 1문풀(과제풀이용/solve.py
+++ 1문풀(과제풀이용/solve.py
@@ -1,6 +1,20 @@
 import sys
+sys.stdin = open('input.txt', 'r')
+
+##################################################
+
+def my_len(arr):
+    cnt = 0
+    for _ in arr:
+        cnt += 1
+    return cnt
 
 T = int(input())
-A, B = map(int, input().split())
 
-print('yes')
+for tc in range(1, T + 1):
+    A, B = input().split()
+
+    # B를 한 글자짜리 특수문자로 치환 -> replace도 쓰면 안되나,,?
+    result = A.replace(B, '*')
+
+    print(f"#{tc} {my_len(result)}")
