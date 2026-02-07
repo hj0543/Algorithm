ca8a7937098b9a47a50ab31c89bfb2a57caf3e53
hj0543 <hj0543@gmail.com>
2026-02-07 09:09:02 +0900

    Update 알고리즘 수업 － 점근적 표기 1.py

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.py
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.py
@@ -5,7 +5,8 @@ x, y = map(int, input().split())
 c = int(input())
 n = int(input())
 
-if n*x + y <= c * n and x <= c:
+if n*x + y <= c * n and x <= c: # c의 기울기가 항상 같거나 더 커야함. 평행할 시 일치하기 때문에 같다가 성립하고 기울기가 더 커야지만 항상 더 크다를 만족시킬 수 있음
     print(1)
 else:
+
     print(0)
