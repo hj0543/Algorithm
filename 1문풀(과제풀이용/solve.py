ac131574889e076f581db868fcd9afb491fae5b0
hj0543 <hj0543@gmail.com>
2026-02-11 17:59:12 +0900

    exit

--- 1문풀(과제풀이용/solve.py
+++ 1문풀(과제풀이용/solve.py
@@ -3,17 +3,11 @@ sys.stdin = open('input.txt', 'r')
 
 ##################################################
 
-TC = int(input())
-
-for tc in range(TC):
-    N = int(input())
-
-    road = []
-    for _ in range(N):
-        road.append(list(map(int, input().split())))
-
-    print(road)
 
 
 
-    # print(f'#{tc+1} {total}')
+
+
+
+
+
---
+++ 5문풀-BOJ 10158/solve.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+##################################################
+
+for tc in range(4):
+    square = list(map(int, input().split()))
+
+    square1 = square[:4]
+    square2 = square[4:]
+
+
+
+
+
+
+
+
+
