734d85e59eaccaf590cb118cc8a984146c8bf9e1
Haneul <hj0543@gmail.com>
2026-02-12 17:51:48 +0900

    go home

--- 1문풀(과제풀이용/solve.py
+++ 1문풀(과제풀이용/solve.py
@@ -3,15 +3,7 @@ sys.stdin = open('input.txt', 'r')
 
 ##################################################
 
-TC = int(input())
-for tc in range(TC):
-    N = int(input())
 
-    farm = []
-    for _ in range(N):
-        farm.append(list(map(int, input().split())))
-
-    for i in range(N):
 
 
 
--- 5문풀-BOJ 10157/solve.py
+++ 5문풀-BOJ 10157/solve.py
@@ -3,25 +3,6 @@ sys.stdin = open('input.txt', 'r')
 
 ##################################################
 
-C, R = map(int, input().split())    # 7 6
-K = int(input())                    # 11
-
-di = [-1, 0, 1, 0]
-dj = [0, 1, 0, -1]
-
-grid = []
-for i in range(R):
-    grid.append([1] * C)
-
-i, j = 0, 0
-k = 0
-while grid[i][j] <= C * R:
-
-    if grid[i][j] += 1
-
-
-
-print(grid)
 
 
 
