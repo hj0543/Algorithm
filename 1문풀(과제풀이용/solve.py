485765808973c54c4f75d2325ba4d028a5f68f2d
Haneul <hj0543@gmail.com>
2026-02-12 14:38:45 +0900

    commit check

--- 1문풀(과제풀이용/solve.py
+++ 1문풀(과제풀이용/solve.py
@@ -3,8 +3,15 @@ sys.stdin = open('input.txt', 'r')
 
 ##################################################
 
+TC = int(input())
+for tc in range(TC):
+    N = int(input())
 
+    farm = []
+    for _ in range(N):
+        farm.append(list(map(int, input().split())))
 
+    for i in range(N):
 
 
 
