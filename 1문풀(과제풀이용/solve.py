8565eab6aa7c81c27ce14c8e1daa65abf72a766c
hj0543 <hj0543@gmail.com>
2026-02-20 17:53:41 +0900

    class end

--- 1문풀(과제풀이용/solve.py
+++ 1문풀(과제풀이용/solve.py
@@ -2,11 +2,34 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 ##################################################
+def enq(n):
+    global last
+    last += 1
+    tree[last] = n
 
+    c = last
+    p = c // 2
+    while p and tree[p] > tree[c]:
+        tree[p], tree[c] = tree[c], tree[p]
+        c = p
+        p = c//2
 
+TC = int(input())
+for tc in range(TC):
+    N = int(input())
+    arr = list(map(int, input().split()))
 
+    tree = [0] * (N + 1)
+    last = 0
 
+    for i in range(N):
+        enq(arr[i])
 
+    total = 0
+    while True:
+        N //= 2
+        total += tree[N]
+        if N == 0:
+            break
 
-
-
+    print(f'#{tc+1}', total)
--- 5문풀-BOJ 10157/solve.py
+++ 5문풀-BOJ 10157/solve.py
@@ -2,6 +2,14 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 ##################################################
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+deque = deque()
+
+arr = list(map(int, input().split()))
 
 
 
