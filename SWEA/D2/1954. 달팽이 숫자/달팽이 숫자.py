193b75fde6c8e4d28fd2dda693011870ed6a3365
hj0543 <hj0543@gmail.com>
2026-03-03 07:17:18 +0900

    [D2] Title: 달팽이 숫자, Time: 61 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,33 +1,31 @@
 TC = int(input())
-
 for tc in range(TC):
-    N = int(input())
-    grid = [[0] * N for _ in range(N)]
+    n = int(input())
+    grid = [[0] * n for _ in range(n)]
 
     dr = [0, 1, 0, -1]
     dc = [1, 0, -1, 0]
-    r, c = 0, 0
-
-    dir_num = [0, 1, 2, 3]
     input_dir = 0
 
 
-    grid[r][c] = 1
-    number = 2
-    for i in range(N ** 2 * 2):
-        cur_dir = (input_dir + 4) % 4
-        nr = r + dr[cur_dir]
-        nc = c + dc[cur_dir]
-        if 0 <= nr < N and 0 <= nc < N:
+    r, c = 0, 0
+    num = 1
+    grid[r][c] = num
+    for i in range((n ** 2) * 2):
+        dir = (input_dir + 4) % 4
+        nr = r + dr[dir]
+        nc = c + dc[dir]
+
+        if 0 <= nr < n and 0 <= nc < n:
             if grid[nr][nc] == 0:
-                grid[nr][nc] = number
+                num += 1
+                grid[nr][nc] = num
                 r, c = nr, nc
-                number += 1
             else:
                 input_dir += 1
         else:
             input_dir += 1
 
     print(f'#{tc+1}')
-    for j in range(N):
-        print(*grid[j])
+    for i in range(n):
+        print(*grid[i])
