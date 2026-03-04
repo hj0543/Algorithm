3568902ab59773e9d0c50f7754f102b27caf669d
hj0543 <hj0543@gmail.com>
2026-03-05 07:55:26 +0900

    0305 start

--- 4-BOJ/solve.py
+++ 4-BOJ/solve.py
@@ -2,44 +2,31 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 ##################################################
-# 2667
+# 1697
 import sys
 input = sys.stdin.readline
-
 from collections import deque
 
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
+n, k = map(int, input().split())
 
-def bfs(sr ,sc):
-    q = deque([(sr, sc)])
-    counts = 0
-    while q:
-        r, c = q.popleft()
-        grid[r][c] = 0
+V = 100000
+visited = [False] * (V + 1)
 
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if 0 <= nr < n and 0 <= nc < n:
-                if grid[nr][nc] != 0:
-                    grid[nr][nc] = 0
-                    counts += 1
-    return counts
+q = deque()
 
-n = int(input())
-grid = []
-for _ in range(n):
-    grid.append(list(map(int,input().rstrip())))
+# (현재 위치, 현재까지 걸린 시간)
+q.append((n, 0))
+visited[n] = True
 
-apt = 0
-houses = []
-for i in range(n):
-    for j in range(n):
-        if grid[i][j] != 0:
-            houses.append(bfs(i, j))
-            apt += 1
+while q:
+    x, time = q.popleft()
 
-print(apt)
-for i in range(apt):
-    print(houses)
+    # 목표 위치에 도착했다면 그 순간의 시간이 최소 시간
+    if x == k:
+        print(time)
+        break
+
+    for nx in (x - 1, x + 1, x * 2):
+        if 0 <= nx <= V and not visited[nx]:
+            visited[nx] = True
+            q.append((nx, time + 1))
