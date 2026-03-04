e3aed757a057a34ea79a413eaab8aa336881627e
hj0543 <hj0543@gmail.com>
2026-03-04 17:46:12 +0900

    class end

--- 1-SWEA/solve.py
+++ 1-SWEA/solve.py
@@ -3,39 +3,44 @@ sys.stdin = open('input.txt', 'r')
 
 ##################################################
 
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
+number_data = {
+    '0001101': 0, '0011001': 1, '0010011': 2, '0111101': 3, '0100011': 4,
+    '0110001': 5, '0101111': 6, '0111011': 7, '0110111': 8, '0001011': 9
+}
 
-def dfs(r, c):
-    if maze[r][c] == 3:
-        return True
+TC = int(input())
+for tc in range(TC):
+    n, m = map(int, input().split())
 
-    for i in range(4):
-        nr = r + dr[i]
-        nc = c + dc[i]
 
-        if 0 <= nr < 16 and 0 <= nc < 16:
-            if maze[nr][nc] != 1 and not visited[nr][nc]:
-                visited[nr][nc] = True
-                if dfs(nr, nc):
-                    return True
-    return False
+    found = False   # 암호 찾았니?
+    grid = []
 
-for _ in range(10):
-    tc = int(input())
-    maze = []
-    for _ in range(16):
-        maze.append(list(map(int,input().rstrip())))
+    for _ in range(n):
+        grid.append(input().rstrip())
+    # print(grid)
+    nums = []
+    for row in grid:
+        if not found and '1' in row:
+            # 암호의 맨 오른쪽 끝(1) 위치 찾기
+            end_idx = row.rfind('1')
 
-    visited = [[False] * 16 for _ in range(16)]
+            # 끝점에서 앞으로 56칸 잘라내기
+            target = row[end_idx - 55: end_idx + 1]
 
-    r, c = 0, 0
-    for i in range(16):
-        for j in range(16):
-            if maze[i][j] == 2:
-                r, c = i, j
+            # 7비트씩 8번 잘라서 숫자로 변환
+            for i in range(0, 56, 7):
+                number = target[i: i + 7]
+                nums.append(number_data[number])
 
-    if dfs(r, c):
-        print(f'#{tc} 1')
+            found = True # 암호 찾음
+        if found:
+            break
+
+    even_number = nums[1], nums[3], nums[5], nums[7]
+    odd_number = nums[0], nums[2], nums[4], nums[6]
+
+    if (sum(odd_number) * 3 + sum(even_number)) % 10 == 0:
+        print(f'#{tc+1} {sum(nums)}')
     else:
-        print(f'#{tc} 0')
+        print(f'#{tc+1} 0')
