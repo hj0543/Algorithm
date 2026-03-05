d9bd25c238f2f40c423ae7b73c0e4deeb216a769
Haneul <hj0543@gmail.com>
2026-03-05 20:51:18 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 652 ms, Memory: 51272 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def topology_sort():
+    result = [1] * (n + 1)
+    q = deque()
+
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        now = q.popleft()
+        # result.append(now)
+
+        for i in graph[now]:
+            indegree[i] -= 1
+            # q.append(i)
+            result[i] = max(result[i], result[now] + 1)
+
+            if indegree[i] == 0:
+                q.append(i)
+
+    print(*result[1:])
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+indegree = [0] * (n + 1)
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+    indegree[e] += 1
+
+topology_sort()
