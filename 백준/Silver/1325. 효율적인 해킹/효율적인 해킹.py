ca39d0eb6e7cf4a68253f27ff7e19fb6658a1a0e
Haneul <hj0543@gmail.com>
2026-03-05 16:12:05 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10220 ms, Memory: 211024 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -0,0 +1,44 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def bfs(s):
+    visited = [False] * (n + 1)
+
+    queue = deque([s])
+
+    visited[s] = True
+    count = 1
+
+    while queue:
+        v = queue.popleft()
+
+        for w in graph[v]:
+            if not visited[w]:
+                visited[w] = True
+                queue.append(w)
+                count += 1
+    return count
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    s, e = map(int, input().split())
+
+    graph[e].append(s)
+
+max_counts = 0
+result = []
+
+for i in range(1, n + 1):
+    cnt = bfs(i)
+
+    if cnt > max_counts:
+        max_counts = cnt
+        result = [i]
+    elif cnt == max_counts:
+        result.append(i)
+
+print(*result)
