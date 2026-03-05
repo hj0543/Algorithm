6603874133889c8a6d4a1a407ebad5a62f4379cc
hj0543 <hj0543@gmail.com>
2026-03-05 22:10:56 +0900

    [Gold III] Title: 줄 세우기, Time: 188 ms, Memory: 40676 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def topology_sort():
+    result = []
+    q = deque()
+
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        now = q.popleft()
+        result.append(now)
+
+        for i in graph[now]:
+            indegree[i] -= 1
+            if indegree[i] == 0:
+                q.append(i)
+    print(*result)
+
+n, m = map(int, input().split())
+indegree = [0] * (n + 1)
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+    indegree[e] += 1    # 이걸 빼먹음;;
+
+topology_sort()
