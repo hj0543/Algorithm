7724ab57fe7ea144bacf1ab81d90a825c54c7fe8
hj0543 <hj0543@gmail.com>
2026-03-05 11:36:26 +0900

    save temp

--- 2-SWEA/solve.py
+++ 2-SWEA/solve.py
@@ -2,46 +2,78 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 ##################################################
-from collections import deque
+# 16진수를 10진수로
+def hex_to_int(tar, base):
+    total_sum = 0
 
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
+    reversed_list = list(tar)
+    reversed_list.reverse()
 
-def bfs(sr, sc):
-    q = deque([(sr, sc)])
+    hex_map = {'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
 
-    while q:
-        r, c = q.popleft()
+    for i in range(len(reversed_list)):
+        char = reversed_list[i]
 
-        if maze[r][c] == 3:
-            return True
+        if base == 16 and char in hex_map:
+            val = hex_map[char]
+        else:
+            val = int(char)
 
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
+        total_sum += val * (base ** i)
 
-            if 0 <= nr < 100 and 0 <= nc < 100:
-                if maze[nr][nc] != 1 and not visited[nr][nc]:
-                    visited[nr][nc] = True
-                    q.append((nr, nc))
+    return total_sum
 
-for _ in range(10):
-    tc = int(input())
-    maze = []
-    for _ in range(100):
-        maze.append(list(map(int, input().rstrip())))
+# 10진수를 2진수로
+def int_to_bi(tar):
+    result = []
 
-    visited = [[False] * 100 for _ in range(100)]
+    while tar != 0:
+        result.append(tar % 2)
+        tar //= 2
 
-    sr, sc = 0, 0
-    for i in range(100):
-        for j in range(100):
-            if maze[i][j] == 2:
-                sr, sc = i, j
+    result.reverse()
 
-    visited[sr][sc] = True
+    while len(result) < 4:
+        result.insert(0, 0)
 
-    if bfs(sr, sc):
-        print(f'#{tc} 1')
-    else:
-        print(f'#{tc} 0')
+    return result
+
+decode_number = {
+    "001101": 0, "010011": 1, "111011": 2, "110001": 3,
+    "100011": 4, "110111": 5, "001011": 6, "111101": 7,
+    "011001": 8, "101111": 9
+}
+
+TC = int(input())
+for tc in range(TC):
+    number_data = input()
+    # print(number_data)
+
+    int_num = []
+    for i in range(len(number_data)):
+        int_num.append(hex_to_int(number_data[i], 16))
+    # print(int_num)
+
+    bi_num = ""
+    for i in range(len(int_num)):
+        bi_num += "".join(map(str, int_to_bi(int_num[i])))
+    # print(bi_num)
+
+    result = []
+    found = False
+    for i in range(len(bi_num) - 5):
+        code = bi_num[i: i + 6]
+
+        if code in decode_number:
+            for j in range(i, len(bi_num)-i, 6):
+                number = bi_num[j:j+6]
+                result.append(number)
+            found = True
+        if found:
+            break
+    # print(result)
+
+    print(f"#{tc + 1}", end=' ')
+    for i in range(len(result)):
+        print(decode_number[result[i]], end=' ')
+    print()
