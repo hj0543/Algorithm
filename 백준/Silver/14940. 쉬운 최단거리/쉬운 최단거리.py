3d356534a6130f2fac2fa1d72a30ec33713ae745
Haneul <hj0543@gmail.com>
2026-03-06 17:50:25 +0900

    Update 쉬운 최단거리.py

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -6,8 +6,8 @@ dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
 def bfs(sr, sc):
+    # 함수 내부에서 큐를 만들고 넘겨받은 시작점을 넣습니다.
     q = deque([(sr, sc)])
-
 
     while q:
         r, c = q.popleft()
@@ -21,7 +21,6 @@ def bfs(sr, sc):
                     visited[nr][nc] = visited[r][c] + 1
                     q.append((nr, nc))
 
-
 n, m = map(int, input().split())
 maze = []
 for _ in range(n):
@@ -30,14 +29,16 @@ for _ in range(n):
 visited = [[-1] * m for _ in range(n)]
 sr, sc = -1, -1
 
+# 지도 전체를 돌며 초기화 작업 진행
 for i in range(n):
     for j in range(m):
         if maze[i][j] == 2:
-            q.append((i, j))
+            sr, sc = i, j      # <--- q.append 대신 시작 좌표를 갱신해 줍니다!
             visited[i][j] = 0
         elif maze[i][j] == 0:
             visited[i][j] = 0
 
+# 찾아낸 시작 좌표를 함수에 넘겨줍니다.
 bfs(sr, sc)
 
 for row in visited:
