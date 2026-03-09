705a77a9b0074532cf82c806e9a9d372d97f691a
hj0543 <hj0543@gmail.com>
2026-03-09 16:08:44 +0900

    class end

--- 1-SWEA/solve.py
+++ 1-SWEA/solve.py
@@ -3,85 +3,48 @@ sys.stdin = open('input.txt', 'r')
 
 ##################################################
 
-# 16진수 -> 2진수 변환 딕셔너리
-hex_to_bin = {
-    '0': '0000', '1': '0001', '2': '0010', '3': '0011',
-    '4': '0100', '5': '0101', '6': '0110', '7': '0111',
-    '8': '1000', '9': '1001', 'A': '1010', 'B': '1011',
-    'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'
-}
+dr = [1, 1, -1, -1]
+dc = [1, -1, -1, 1]
 
-# 암호코드 가로 길이비
-ratio = {
-    (2, 1, 1): 0, (2, 2, 1): 1, (1, 2, 2): 2, (4, 1, 1): 3,
-    (1, 3, 2): 4, (2, 3, 1): 5, (1, 1, 4): 6, (3, 1, 2): 7,
-    (2, 1, 3): 8, (1, 1, 2): 9
-}
+
+def dfs(r, c, d, count):
+    global max_desserts, sr, sc
+
+    if d == 3 and r == sr and c == sc:  # d == 3 없으면 그대로 종료 됨...
+        max_desserts = max(max_desserts, count)
+        return
+
+    # 방향전환로직
+    for i in range(d, d + 2):
+        if i < 4:
+            nr, nc = r + dr[i], c + dc[i]
+
+            if 0 <= nr < n and 0 <= nc < n:
+                # 출발점으로 돌아온 경우
+                if nr == sr and nc == sc:
+                    dfs(nr, nc, i, count)
+
+                # 처음 먹어보는 디저트인 경우
+                elif not visited[grid[nr][nc]]:
+                    visited[grid[nr][nc]] = True
+                    dfs(nr, nc, i, count + 1)
+                    visited[grid[nr][nc]] = False
+
 
 TC = int(input())
 for tc in range(TC):
-    N, M = map(int, input().split())
+    n = int(input())
+    grid = [list(map(int, input().split())) for _ in range(n)]
 
-    grid = []
-    for _ in range(N):
-        grid.append(list(map(str, input().rstrip())))
-    # print(grid)
+    max_desserts = -1
+    visited = [False] * 101
 
-    visited_codes = []  # 코드 저장하는 리스트
-    total_sum = 0
+    # 시작점 설정
+    for r in range(n - 2):
+        for c in range(1, n - 1):
+            sr, sc = r, c
+            visited[grid[r][c]] = True
+            dfs(r, c, 0, 1)  # (현재 r, 현재 c, 현재 방향, 먹은 개수)
+            visited[grid[r][c]] = False
 
-    for row in grid:
-        # 16진수를 2진수 문자열로 변환
-        bin_num = ""
-        for char in row:
-            bin_num += hex_to_bin[char]
-
-        # print(bin_num)
-        i = len(bin_num) - 1
-
-        nums = []
-
-        while i >= 0:
-            if bin_num[i] == '1':
-                c3 = c2 = c1 = 0
-
-                # 맨 뒤 1의 연속 개수 카운트
-                while i >= 0 and bin_num[i] == '1':
-                    c3 += 1
-                    i -= 1
-                # 그 앞 0의 연속 개수 카운트
-                while i >= 0 and bin_num[i] == '0':
-                    c2 += 1
-                    i -= 1
-                # 그 앞 1의 연속 개수 카운트
-                while i >= 0 and bin_num[i] == '1':
-                    c1 += 1
-                    i -= 1
-
-                # 배율 알아내기
-                # 얘가 원래 비임
-                min_c = min(c1, c2, c3)
-                # 간단한 자연수의 비
-                r1, r2, r3 = c1 // min_c, c2 // min_c, c3 // min_c
-                # 비율을 넣음
-                nums.append(ratio[(r1, r2, r3)])
-
-                if len(nums) == 8:      # 숫자 8개를 모두 찾았다면
-                    nums = nums[::-1]   # 뒤에서부터 찾았으므로 리스트를 뒤집어줌
-
-                    # 같은 코드가 여러줄 있으니까 처음 발견한 암호코드 배열인지 확인
-                    if nums not in visited_codes:
-                        visited_codes.append(nums)  # 중복 방지를 위해 하나만 저장
-
-                        odd_sum = nums[0] + nums[2] + nums[4] + nums[6]
-                        even_sum = nums[1] + nums[3] + nums[5]
-                        verify_num = nums[7]
-
-                        if ((odd_sum * 3) + even_sum + verify_num) % 10 == 0:
-                            total_sum += sum(nums)
-
-                    nums = []  # 같은 줄에 코드 더 있을 수 있으니 초기화
-            else:
-                i -= 1  # 못찾았으면 왼쪽으로 이동
-
-    print(f"#{tc+1} {total_sum}")
+    print(f"#{tc+1} {max_desserts}")
--- 4-BOJ/solve.py
+++ 4-BOJ/solve.py
@@ -2,50 +2,24 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 ##################################################
-# 17086
-import sys
-input = sys.stdin.readline
-from collections import deque
-
-dr = [-1, 0, 1, 0, -1, 1, -1, 1]
-dc = [0, 1, 0, -1, 1, 1, -1, -1]
-
-def bfs(starts):
-    q = deque(starts)
-
-    for sr, sc in starts:
-        visited[sr][sc] = 0
-
-    while q:
-        r, c = q.popleft()
-
-        for i in range(8):
-            nr = r + dr[i]
-            nc = c + dc[i]
-
-            if 0 <= nr < n and 0 <= nc < m:
-                if grid[nr][nc] == 0 and visited[nr][nc] == -1:
-                    visited[nr][nc] = visited[r][c] + 1
-                    q.append((nr, nc))
-
+# 15652
 n, m = map(int, input().split())
-grid = []
-for _ in range(n):
-    grid.append(list(map(int, input().split())))
+result = [0] * m
 
-visited = [[-1] * m for _ in range(n)]
+def backtracking(depth):
+    if depth == m:
+        # check = 1
+        # for i in range(1, m):
+        #     if result[i - 1] <= result[i]:
+        #         check += 1
+        # if check == m:
+        #     print(*result, sep = ' ')
+        return
 
-sharks = []
-for i in range(n):
-    for j in range(m):
-        if grid[i][j] == 1:
-            sharks.append((i, j))
+    for i in range(1, n + 1):
+        result[depth] = i
+        backtracking(depth + 1)
 
-bfs(sharks)
+backtracking(0)
 
-max_safety_length = 0
-for i in range(n):
-    if max_safety_length < max(visited[i]):
-        max_safety_length = max(visited[i])
 
-print(max_safety_length)
