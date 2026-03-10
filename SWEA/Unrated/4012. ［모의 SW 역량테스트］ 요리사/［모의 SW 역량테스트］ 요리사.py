755391661f4ded4624a27e0fa1c30d56b4df207f
hj0543 <hj0543@gmail.com>
2026-03-10 15:57:01 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 582 ms, Memory: 62,080 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
@@ -1,34 +1,41 @@
+# [로직전략]
+# 1. 2개를 뽑는 모든 경우의 수를 구해서 시너지 계산하기 (itertools 사용해도 됨?)
+# 2. A, B 따로 담아두기
+# 3. 완탐으로 순회하면서 최소값 갱신
 from itertools import combinations
 
-def synergy(ingredients, matrix):
-    score = 0
+def synergy(ingredients, grid):
+    flavor = 0
     # 그룹 내에서 2개를 뽑는 모든 조합
     for i, j in combinations(ingredients, 2):
-        score += matrix[i][j] + matrix[j][i]
-    return score
+        flavor += grid[i][j] + grid[j][i]
+    return flavor
+
 
 TC = int(input())
 
 for tc in range(TC):
     N = int(input())
-    matrix = [list(map(int, input().split())) for _ in range(N)]
-    
+    grid = [list(map(int, input().split())) for _ in range(N)]
+
     # 0부터 N-1
     all_ingredients = list(range(N))
     min_diff = float('inf')
 
     # N개 중 N/2개를 뽑는 모든 조합 순회
-    for food_a in combinations(all_ingredients, N // 2):
-        # A에 속하지 않은 나머지가 food_b
-        food_b = [x for x in all_ingredients if x not in food_a]
+    for A in combinations(all_ingredients, N // 2):
         
-        # 각 음식의 맛 계산
-        score_a = synergy(food_a, matrix)
-        score_b = synergy(food_b, matrix)
-        
-        # 차이의 최솟값 갱신
-        diff = abs(score_a - score_b)
-        if diff < min_diff:
-            min_diff = diff
+        B = []  # 나머지가 B
+        for x in all_ingredients:
+            if x not in A:
+                B.append(x)
 
-    print(f"#{tc+1} {min_diff}")
+        # 각 음식의 맛 계산
+        flavor_a = synergy(A, grid)
+        flavor_b = synergy(B, grid)
+
+        # 차이의 최솟값 갱신
+        diff = abs(flavor_a - flavor_b)
+        min_diff = min(diff, min_diff)
+
+    print(f"#{tc + 1} {min_diff}")
