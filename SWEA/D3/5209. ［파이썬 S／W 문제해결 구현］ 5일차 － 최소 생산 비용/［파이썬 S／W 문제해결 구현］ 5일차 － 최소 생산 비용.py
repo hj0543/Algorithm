3b1dba064b3e9a32c30ac9295ae8ebbc46bdf650
Haneul <hj0543@gmail.com>
2026-03-13 01:09:28 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 5일차 - 최소 생산 비용, Time: 380 ms, Memory: 61,824 KB -BaekjoonHub

--- SWEA/D3/5209. ［파이썬 S／W 문제해결 구현］ 5일차 － 최소 생산 비용/［파이썬 S／W 문제해결 구현］ 5일차 － 최소 생산 비용.py
+++ SWEA/D3/5209. ［파이썬 S／W 문제해결 구현］ 5일차 － 최소 생산 비용/［파이썬 S／W 문제해결 구현］ 5일차 － 최소 생산 비용.py
@@ -3,34 +3,34 @@
 # 1. 가지치기 : 현재까지의 비용이 이미 구한 최소 비용 이상이면 더 이상 탐색하지 않고 return
 # 2. 종료조건 : N개의 제품에 대한 공장 배정이 모두 끝났다면 최소 비용을 갱신하고 return
 # 3. 탐색 : 아직 배정되지 않은 공장(visited 체크)에 대해 배정 후 다음 제품(row+1)으로 재귀 호출, 이후 상태 복구(백트래킹)
-
+ 
 def dfs(row, current_cost):
     global min_cost
-    
+     
     # 1. 가지치기
     if current_cost >= min_cost:
         return
-        
+         
     # 2. 종료조건
     if row == N:
         min_cost = current_cost
         return
-        
+         
     # 3. 공장 탐색 및 배정 (백트래킹)
     for col in range(N):
         if not visited[col]:
             visited[col] = 1
             dfs(row + 1, current_cost + cost[row][col])
             visited[col] = 0 # 상태 복구
-
+ 
 TC = int(input())
 for tc in range(TC):
     N = int(input())
     cost = [list(map(int, input().split())) for _ in range(N)]
-    
+     
     visited = [0] * N
     min_cost = float('inf')
-    
+     
     dfs(0, 0)
-    
+     
     print(f'#{tc+1} {min_cost}')
