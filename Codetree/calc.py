7cdf73bd68162f17f173cfb820f2fe1c25e1e4ff
hj0543 <hj0543@gmail.com>
2026-03-19 17:41:31 +0900

    leave

--- Codetree/calc.py
+++ Codetree/calc.py
@@ -1,12 +1,27 @@
 
-arr = [6,3,2,7,8,11,10,9]
+# arr = [73,80,70,77,75]
 
-avg = sum(arr) / len(arr)
+# avg = sum(arr) / len(arr)
 
-boonsan = []
-for i in range(len(arr)):
-    boonsan.append((arr[i] - avg) ** 2)
+# # 편차
+# deviation = []
+# for i in range(len(arr)):
+#     deviation.append((arr[i] - avg))
 
-result = (sum(boonsan) / len(arr)) ** 0.5
 
-print(result)
+# # 편차제곱
+# deviation_squared = []
+# for i in range(len(arr)):
+#     deviation_squared.append(deviation[i] ** 2)
+
+# # 분산
+# boonsan = (sum(deviation_squared) / len(arr))
+
+# # 표준편차
+# standard_deviation = boonsan ** 0.5
+
+
+# result = []
+# for i in arr:
+#     result.append(i - avg)
+# print(boonsan)
