c88cd08ca228a2cf5ef245b8eb615f7e366c29a4
hj0543 <hj0543@gmail.com>
2026-04-08 01:21:16 +0900

    [Gold I] Title: 뭉쳐야 산다, Time: 1876 ms, Memory: 190088 KB -BaekjoonHub

---
+++ 백준/Gold/28277. 뭉쳐야 산다/뭉쳐야 산다.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+N, Q = map(int, input().split())
+
+result = [set() for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    data = list(map(int, input().split()))
+    k = data[0]
+    if k:
+        result[i] = set(data[1:])
+
+out = []
+
+for _ in range(Q):
+    cmd, a, *rest = map(int, input().split())
+    
+    # 집합 Sa를 Sa ∪ Sb로 바꾸고, Sb는 공집합으로 바꾼다.
+    if cmd == 1:
+        b = rest[0]
+
+        if len(result[a]) < len(result[b]):
+            result[a], result[b] = result[b], result[a]
+
+        result[a].update(result[b])
+        result[b].clear()
+    
+    # 집합 Sa의 크기를 출력한다.
+    else:
+        out.append(str(len(result[a])))
+
+for ans in out:
+    print(ans)
