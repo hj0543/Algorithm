6f24ed37d1e3337c70beeac47fef9f645157cd62
hj0543 <hj0543@gmail.com>
2026-04-08 01:03:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 788 ms, Memory: 61,568 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
@@ -6,29 +6,29 @@ def synergy(mask):
             if (mask & (1 << i)) and (mask & (1 << j)):
                 flavor += grid[i][j] + grid[j][i]
     return flavor
-
-
+ 
+ 
 TC = int(input())
-
+ 
 for tc in range(TC):
     N = int(input())
     grid = [list(map(int, input().split())) for _ in range(N)]
-
+ 
     min_diff = float('inf')
-
+ 
     # 모든 부분집합 중 재료 개수가 N // 2개인 경우만 선택
     for mask in range(1 << N):
         if bin(mask).count('1') != N // 2:
             continue
-
+ 
         # A 음식에 들어갈 재료 집합
         A = mask
         # B 음식은 전체 집합에서 A를 제외한 나머지 재료 집합
         B = ((1 << N) - 1) ^ mask
-
+ 
         flavor_a = synergy(A)
         flavor_b = synergy(B)
-
+ 
         min_diff = min(min_diff, abs(flavor_a - flavor_b))
-
+ 
     print(f"#{tc + 1} {min_diff}")
