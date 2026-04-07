b0981a4e3b173a8a661f2624cdbe4ce805f9be17
hj0543 <hj0543@gmail.com>
2026-04-07 15:23:24 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 788 ms, Memory: 61,568 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
@@ -1,14 +1,10 @@
-# [로직전략]
-# 1. 2개를 뽑는 모든 경우의 수를 구해서 시너지 계산하기 (itertools 사용해도 됨?)
-# 2. A, B 따로 담아두기
-# 3. 완탐으로 순회하면서 최소값 갱신
-from itertools import combinations
-
-def synergy(ingredients, grid):
+def synergy(mask):
+    # mask에 포함된 재료들끼리의 시너지 합을 계산
     flavor = 0
-    # 그룹 내에서 2개를 뽑는 모든 조합
-    for i, j in combinations(ingredients, 2):
-        flavor += grid[i][j] + grid[j][i]
+    for i in range(N):
+        for j in range(i + 1, N):
+            if (mask & (1 << i)) and (mask & (1 << j)):
+                flavor += grid[i][j] + grid[j][i]
     return flavor
 
 
@@ -18,24 +14,21 @@ for tc in range(TC):
     N = int(input())
     grid = [list(map(int, input().split())) for _ in range(N)]
 
-    # 0부터 N-1
-    all_ingredients = list(range(N))
     min_diff = float('inf')
 
-    # N개 중 N/2개를 뽑는 모든 조합 순회
-    for A in combinations(all_ingredients, N // 2):
-        
-        B = []  # 나머지가 B
-        for x in all_ingredients:
-            if x not in A:
-                B.append(x)
+    # 모든 부분집합 중 재료 개수가 N // 2개인 경우만 선택
+    for mask in range(1 << N):
+        if bin(mask).count('1') != N // 2:
+            continue
 
-        # 각 음식의 맛 계산
-        flavor_a = synergy(A, grid)
-        flavor_b = synergy(B, grid)
+        # A 음식에 들어갈 재료 집합
+        A = mask
+        # B 음식은 전체 집합에서 A를 제외한 나머지 재료 집합
+        B = ((1 << N) - 1) ^ mask
 
-        # 차이의 최솟값 갱신
-        diff = abs(flavor_a - flavor_b)
-        min_diff = min(diff, min_diff)
+        flavor_a = synergy(A)
+        flavor_b = synergy(B)
+
+        min_diff = min(min_diff, abs(flavor_a - flavor_b))
 
     print(f"#{tc + 1} {min_diff}")
